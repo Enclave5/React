b2ff26bc13dfbc4b18237ef6f08567864218b036
Арсланов Кирилл <kirillarslanov00@gmail.com>
2024-01-19 09:23:06 +0300

    application output without using JSX

--- src/App.js
+++ src/App.js
@@ -9,27 +9,41 @@ function CurrentDateTime() {
 //eniso.ru/javascript-i-rabota-s-datoy-i-vremenem-realizatsiya-vyvoda-daty-i-vremeni-na-sayt
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{CurrentDateTime()}
-			</header>
-		</div>
+	return /*#__PURE__*/ React.createElement(
+		"div",
+		{
+			className: "App",
+		},
+		/*#__PURE__*/ React.createElement(
+			"header",
+			{
+				className: "App-header",
+			},
+			/*#__PURE__*/ React.createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: "logo",
+			}),
+			/*#__PURE__*/ React.createElement(
+				"p",
+				null,
+				"Edit ",
+				/*#__PURE__*/ React.createElement("code", null, "src/App.js"),
+				" and save to reload."
+			),
+			/*#__PURE__*/ React.createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			CurrentDateTime()
+		)
 	);
 }
-
-// export default CurrentDateTime;
 
 export default App;
